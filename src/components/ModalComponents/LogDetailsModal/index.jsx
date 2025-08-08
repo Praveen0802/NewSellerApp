319db9b2dde6ba9b39d596b849d787252427844c
Santosh W <santosh@1boxoffice.ae>
2025-08-08 18:42:08 +0530

    add inventory  changes

--- src/components/ModalComponents/LogDetailsModal/index.jsx
+++ src/components/ModalComponents/LogDetailsModal/index.jsx
@@ -262,7 +262,7 @@ const LogDetailsModal = ({
           <button
             className={`px-6 py-3 text-sm font-medium transition-colors duration-200 ${
               activeTab === "order"
-                ? "text-gray-600 border-b-2 border-blue-600 bg-blue-50"
+                ? "text-gray-600 border-b-2 border-green-600 bg-blue-50"
                 : "text-gray-500 hover:text-gray-700 hover:bg-gray-50"
             }`}
             onClick={() => {
@@ -275,7 +275,7 @@ const LogDetailsModal = ({
           <button
             className={`px-6 py-3 text-sm font-medium transition-colors duration-200 ${
               activeTab === "inventory"
-                ? "text-gray-600 border-b-2 border-blue-600 bg-blue-50"
+                ? "text-gray-600 border-b-2 border-green-600 bg-blue-50"
                 : "text-gray-500 hover:text-gray-700 hover:bg-gray-50"
             }`}
             onClick={() => {
