e69fdbe13c73ddd7d206f71bf8814cd167b0d502
Ashike <65408682+Ashikes@users.noreply.github.com>
2025-09-02 16:37:33 +0530

    approved success message remove

--- src/components/KYC/KycComponent/index.jsx
+++ src/components/KYC/KycComponent/index.jsx
@@ -1038,7 +1038,7 @@ const MobileSubmitButton = () => {
             {Object.keys(documentConfig).map(renderDocumentCard)}
 
             {/* Success Message */}
-            {approvedDocuments === totalDocuments && (
+            {/* {approvedDocuments === totalDocuments && (
               <div className="bg-green-50 border border-green-200 rounded-lg p-4">
                 <div className="flex items-center space-x-3">
                   <Check className="w-5 h-5 text-green-600" />
@@ -1052,7 +1052,7 @@ const MobileSubmitButton = () => {
                   </div>
                 </div>
               </div>
-            )}
+            )} */}
           </div>
             {/* Centered compact submit button below cards */}
             <div className="flex justify-center">
